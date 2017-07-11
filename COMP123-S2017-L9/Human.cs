cb7c5a84354f0e4d6c311ef8575c7cdaf3d9b4ab
Angelaaa <Angelaaa@LAPTOP-0Q8KPPTN>
2017-07-11 17:35:47 -0400

    Add SuperHuman Class

--- COMP123-S2017-L9/Human.cs
+++ COMP123-S2017-L9/Human.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace COMP123_S2017_L9
-{
 /*
  * Name: Yanying Zhang
  * Date: July 11, 2017
  * Description: Demo for Lesson 9
  * Version: 0.1 - Create the Progam class
  */
+namespace COMP123_S2017_L9
+{
     public abstract class Human
     {
         // PRIVATE PROPERTIES (FIELDS)
---
+++ COMP123-S2017-L9/SuperHuman.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Yanying Zhang
+ * Date: July 11, 2017
+ * Description: Demo for Lesson 9
+ * Version: 0.1 - Create the Progam class
+ */
+namespace COMP123_S2017_L9
+{
+    /// <summary>
+    /// This is the SuperHuman class.
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        //PRIVATE INSTANCE VARIALES
+
+
+        //PUBLIC PROPERTIES
+
+        //CONSTRUCTORS
+        public SuperHuman(string name)
+            :base(name)
+        {
+
+        }
+        //PRIVATE METHODS
+
+        //PUBLIC METHODS
+
+    }
+}
